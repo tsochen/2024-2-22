8fe5133c18e6dd6f2fe387dbe9374eb3d2bcd208
座丞 <160603078+tsochen@users.noreply.github.com>
2024-05-07 10:03:26 +0000

    modify

--- lesson20/lesson20_2.py
+++ lesson20/lesson20_2.py
@@ -1,7 +1,41 @@
-from flask import Flask
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import *
+import os
+import google.generativeai as genai
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, 李座丞!</p>"
+line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
+handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
+genai.configure(api_key=os.environ['Gemini_API_KEY'])
+model = genai.GenerativeModel('gemini-pro')
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    try:
+        response = model.generate_content(event.message.text)
+        message = TextSendMessage(text=response.text)
+        line_bot_api.reply_message(event.reply_token, message)
+    except:
+        message = TextSendMessage(text="發生不明錯誤!請等一下再試")
+        line_bot_api.reply_message(event.reply_token, message)
+
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
